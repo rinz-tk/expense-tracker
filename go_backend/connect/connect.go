1b065df52c6b8d56e49bac77483138a3c0415a70
rinz-tk <tahamkhan0701@gmail.com>
2025-11-25 16:58:30 -0800

    registry struct

--- go_backend/connect/connect.go
+++ go_backend/connect/connect.go
@@ -11,28 +11,57 @@ const ROOT = ".."
 const INDEX = "/react_frontend/dist/index.html"
 const API = "api/"
 
+type RegistryVal struct {
+	password string
+	uid uint32
+}
+
+type RegistryRead struct {
+	Username string
+	WriteTo chan<- RegistryVal
+}
+
+type RegistryWrite struct {
+	Username string
+	WriteVal RegistryVal
+}
+
 type Connect struct {
 	Id uint32
+
+	RegistryReadSend chan<- RegistryRead
+	RegistryReadRecv <-chan RegistryVal
+	RegistryWriteSend chan<- RegistryWrite
 }
 
 func (c *Connect) Log(msg string) {
 	fmt.Printf("[Request %v] %v\n", c.Id, msg)
 }
 
+func (c *Connect) trigger_endpoint(w http.ResponseWriter, r *http.Request) {
+	link := r.URL.Path
+	endpoint := strings.TrimPrefix(link, "/" + API)
+
+	switch {
+	case r.Method == "POST" && endpoint == "register":
+		c.Log("Endpoint register triggered")
+	}
+}
+
 func (c *Connect) Route(w http.ResponseWriter, r *http.Request) {
 	link := r.URL.Path
-	split := strings.SplitAfterN(link, "/", 2)
+	split := strings.SplitAfterN(link, "/", 3)
 
 	switch split[1] {
 		case "":
 			c.Log("Requested index")
 			http.ServeFile(w, r, fmt.Sprintf("%v%v", ROOT, INDEX))
 
-		// case API:
-		// 	c.trigger_endpoint(w, r)
+		case API:
+			c.trigger_endpoint(w, r)
 
 		default:
-			c.Log(fmt.Sprintf("Requested file: %v", split[1]))
+			c.Log(fmt.Sprintf("Requested file: %v", link))
 			http.ServeFile(w, r, fmt.Sprintf("%v%v", ROOT, link))
 	}
 }
--- go_backend/main.go
+++ go_backend/main.go
@@ -16,15 +16,36 @@ func gen_id(id_ch chan<- uint32) {
 	}
 }
 
+func manage_registry(read_chan <-chan con.RegistryRead, write_chan <-chan con.RegistryWrite) {
+	registry := make(map[string]con.RegistryVal)
+
+	for {
+		select {
+		case read := <-read_chan:
+			read.WriteTo <- registry[read.Username]
+		case write := <-write_chan:
+			registry[write.Username] = write.WriteVal
+		}
+	}
+}
+
 func main() {
 	id_ch := make(chan uint32)
+	registry_read_chan := make(chan con.RegistryRead)
+	registry_write_chan := make(chan con.RegistryWrite)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		c := con.Connect { Id: <-id_ch }
+		c := con.Connect {
+			Id: <-id_ch,
+			RegistryReadSend: registry_read_chan,
+			RegistryReadRecv: make(chan con.RegistryVal),
+			RegistryWriteSend: registry_write_chan,
+		}
 		c.Route(w, r)
 	})
 
 	go gen_id(id_ch)
+	go manage_registry(registry_read_chan, registry_write_chan)
 
 	addr := "127.0.0.1:3003"
 	fmt.Printf("Connect to -> http://%v\n", addr)
