a8af2998da0defc09de88f57999b47b414d74d73
rinz-tk <tahamkhan0701@gmail.com>
2025-11-27 01:38:59 -0800

    go register endpoint

--- go_backend/connect/connect.go
+++ go_backend/connect/connect.go
@@ -1,9 +1,11 @@
 package connect
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
+	"encoding/json"
 	"net/http"
 )
 
@@ -16,9 +18,14 @@ type RegistryVal struct {
 	uid uint32
 }
 
+type RegistryReadVal struct {
+	Val RegistryVal
+	Ok bool
+}
+
 type RegistryRead struct {
 	Username string
-	WriteTo chan<- RegistryVal
+	WriteTo chan RegistryReadVal
 }
 
 type RegistryWrite struct {
@@ -26,26 +33,83 @@ type RegistryWrite struct {
 	WriteVal RegistryVal
 }
 
+type RegistryCheck struct {
+	Username string
+	WriteTo chan bool
+}
+
+type RegisterInfo struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RegisterReturn struct {
+	status string
+	content string
+}
+
 type Connect struct {
 	Id uint32
+	UidChan chan uint32
 
-	RegistryReadSend chan<- RegistryRead
-	RegistryReadRecv <-chan RegistryVal
-	RegistryWriteSend chan<- RegistryWrite
+	RegistryReadSend chan RegistryRead
+	RegistryReadRecv chan RegistryVal
+	RegistryWriteSend chan RegistryWrite
+	RegistryCheckSend chan RegistryCheck
+	RegistryCheckRecv chan bool
 }
 
 func (c *Connect) Log(msg string) {
 	fmt.Printf("[Request %v] %v\n", c.Id, msg)
 }
 
-func (c *Connect) trigger_endpoint(w http.ResponseWriter, r *http.Request) {
+func (c *Connect) register_user(r *http.Request) (RegisterReturn, error) {
+	var reg_info RegisterInfo
+	if err := json.NewDecoder(r.Body).Decode(&reg_info); err != nil {
+		c.Log("Error while parsing JSON")
+		return RegisterReturn{}, err
+	}
+
+	c.RegistryCheckSend <- RegistryCheck{
+		Username: reg_info.Username,
+		WriteTo: c.RegistryCheckRecv,
+	}
+
+	if ok := <-c.RegistryCheckRecv; ok {
+		c.Log(fmt.Sprintf("Username '%v' already exists", reg_info.Username))
+		return RegisterReturn{ status: "Exists" }, nil
+	}
+
+	id := <-c.UidChan
+
+	c.Log(fmt.Sprintf("Adding username '%v' with password '%v' and uid '%v'", reg_info.Username, reg_info.Password, id))
+	c.RegistryWriteSend <- RegistryWrite{ reg_info.Username, RegistryVal{ reg_info.Password, id } }
+
+	return RegisterReturn{}, errors.New("TODO")
+}
+
+func (c *Connect) trigger_endpoint(w http.ResponseWriter, r *http.Request) error {
 	link := r.URL.Path
 	endpoint := strings.TrimPrefix(link, "/" + API)
 
+	var out []byte
 	switch {
 	case r.Method == "POST" && endpoint == "register":
 		c.Log("Endpoint register triggered")
+
+		data, err := c.register_user(r)
+		if err != nil { return err }
+
+		out, err = json.Marshal(data)
+		if err != nil { return err }
+
+	default:
+		err_msg := fmt.Sprintf("Route doesn't exist: %v", link)
+		return errors.New(err_msg)
 	}
+
+	w.Write(out)
+	return nil
 }
 
 func (c *Connect) Route(w http.ResponseWriter, r *http.Request) {
@@ -58,7 +122,10 @@ func (c *Connect) Route(w http.ResponseWriter, r *http.Request) {
 			http.ServeFile(w, r, fmt.Sprintf("%v%v", ROOT, INDEX))
 
 		case API:
-			c.trigger_endpoint(w, r)
+			if err := c.trigger_endpoint(w, r); err != nil {
+				c.Log(fmt.Sprintf("Error: %v", err.Error()))
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
 
 		default:
 			c.Log(fmt.Sprintf("Requested file: %v", link))
--- go_backend/main.go
+++ go_backend/main.go
@@ -16,36 +16,46 @@ func gen_id(id_ch chan<- uint32) {
 	}
 }
 
-func manage_registry(read_chan <-chan con.RegistryRead, write_chan <-chan con.RegistryWrite) {
+func manage_registry(read_chan <-chan con.RegistryRead, write_chan <-chan con.RegistryWrite, check_chan <-chan con.RegistryCheck) {
 	registry := make(map[string]con.RegistryVal)
 
 	for {
 		select {
 		case read := <-read_chan:
-			read.WriteTo <- registry[read.Username]
+			val, ok := registry[read.Username]
+			read.WriteTo <- con.RegistryReadVal{ Val: val, Ok: ok }
 		case write := <-write_chan:
 			registry[write.Username] = write.WriteVal
+		case check := <-check_chan:
+			_, ok := registry[check.Username]
+			check.WriteTo <- ok
 		}
 	}
 }
 
 func main() {
 	id_ch := make(chan uint32)
+	uid_ch := make(chan uint32)
 	registry_read_chan := make(chan con.RegistryRead)
 	registry_write_chan := make(chan con.RegistryWrite)
+	registry_check_chan := make(chan con.RegistryCheck)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		c := con.Connect {
 			Id: <-id_ch,
+			UidChan: uid_ch,
 			RegistryReadSend: registry_read_chan,
 			RegistryReadRecv: make(chan con.RegistryVal),
 			RegistryWriteSend: registry_write_chan,
+			RegistryCheckSend: registry_check_chan,
+			RegistryCheckRecv: make(chan bool),
 		}
 		c.Route(w, r)
 	})
 
 	go gen_id(id_ch)
-	go manage_registry(registry_read_chan, registry_write_chan)
+	go gen_id(uid_ch)
+	go manage_registry(registry_read_chan, registry_write_chan, registry_check_chan)
 
 	addr := "127.0.0.1:3003"
 	fmt.Printf("Connect to -> http://%v\n", addr)
